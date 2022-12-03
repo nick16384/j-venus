e12703b0b79f39a40faeac4b46387acf7ca0be94
nick16384 <119735278+nick16384@users.noreply.github.com>
2022-12-03 12:43:40 +0100

    Add files via upload

---
+++ data/commands/rickroll.java
@@ -0,0 +1,12 @@
+//Type:JDEXE:JavaDOS Executable
+//Format:JD_CF1.0:JavaDOS_CommandFormat_1.0
+
+
+//CODE:
+public class rickroll {
+	public static void rickroll() {
+		//Main.cmdLine.append("Never gonne give you up!");
+		System.err.println("Never gonna give you up.");
+		System.out.println("Rickroll successful!");
+	}
+}
---
+++ data/temp/rickroll.java
@@ -0,0 +1,7 @@
+
+public class rickroll {
+	public static void rickroll() {
+		//Main.cmdLine.append("Never gonne give you up!");
+		System.out.println("Rickroll successful!");
+	}
+}
