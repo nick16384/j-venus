dc32f3ac32c11baf58e588b9d5dba761d9edece8
nick16384 <theophil@pudelkern.com>
2023-05-02 20:11:55 +0200

    Change-Id: I0000000000000000000000000000000000000000
    Change-Id: I337ab061526f9d99d5aec305953e018d63d1cbd0

--- J-Venus/src/jfx/windowManager/JFxWinloader.java
+++ J-Venus/src/jfx/windowManager/JFxWinloader.java
@@ -1,17 +1,27 @@
 package jfx.windowManager;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
 import engine.sys;
 import javafx.scene.Scene;
 import javafx.scene.control.TextArea;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.Region;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.application.Application;
 import javafx.application.Platform;
-import javafx.beans.property.StringProperty;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.stage.Stage;
+import libraries.Err;
+import libraries.OpenLib;
 import libraries.VarLib;
 import main.Main;
 
@@ -40,13 +50,41 @@ public class JFxWinloader extends Application {
 			primaryStage.setTitle("J-Venus " + VarLib.getVersion());
 			Main.cmdLine = new TextArea();
 			Main.cmdLine.setWrapText(true);
-			Main.cmdLine.setText("SHELL INIT\n Another bit of text");
-			Main.cmdLine.setStyle("-fx-text-fill: green");
-			Main.cmdLine.setStyle("-fx-background-color: #aabbcc");
-			Main.cmdLine.setStyle("-fx-font: 12pt Consolas");
+			Main.cmdLine.setText("SHELL INIT");
+			Main.cmdLine.setStyle(""
+					+ "-fx-control-inner-background:#000000;"
+					//+ " -fx-font-family: Terminus(TTF);"
+					//+ " -fx-font: 12pt Terminus(TTF);" //Font is set below
+					+ " -fx-highlight-fill: #00ff00;"
+					+ " -fx-highlight-text-fill: #000000;"
+					+ " -fx-text-fill: #00ff00; ");
+			sys.log("\n" + Main.cmdLine.getStyle());
+			Main.cmdLine.setFont(new Font("Terminus (TTF)", 18));
+			
 			Main.cmdLine.relocate(0, 0);
 			
-			//TODO add Event Listener for Enter input
+			Main.cmdLine.setOnKeyPressed(event -> {
+				if (event.getCode().equals(KeyCode.ENTER)) {
+					// ON COMMAND SUBMIT =================================================
+		        	actionOnEnter();
+		        	// COMMAND SUBMIT END ================================================
+				} else if (event.getCode().equals(KeyCode.PLUS)) {
+					sys.log("JFX", 1, "Increasing shell font size.");
+					Main.cmdLine.setFont(new Font("Terminus (TTF)", Main.cmdLine.getFont().getSize() + 1));
+					// Remove last character
+					Main.cmdLine.setText(Main.cmdLine.getText().substring(0, Main.cmdLine.getText().length() - 1));
+					Main.cmdLine.positionCaret(Main.cmdLine.getText().length());
+				} else if (event.getCode().equals(KeyCode.MINUS)) {
+					sys.log("JFX", 1, "Shrinking shell font size.");
+					Main.cmdLine.setFont(new Font("Terminus (TTF)", Main.cmdLine.getFont().getSize() - 1));
+					// Remove last character
+					Main.cmdLine.setText(Main.cmdLine.getText().substring(0, Main.cmdLine.getText().length() - 1));
+					Main.cmdLine.positionCaret(Main.cmdLine.getText().length());
+				}
+				
+				//TODO Add support for command repeat when pressing "up" key
+			});
+			
 			//TODO change style to match a console
 
 			StackPane root = new StackPane();
@@ -67,7 +105,7 @@ public class JFxWinloader extends Application {
 	}
 	
 	public void appendText(String text) {
-		sys.log("JFX", 1, "Appending new text to cmdLine. Length: " + text.length());
+		sys.log("JFX", 1, "Appending new text to cmdLine with " + text.length() + " characters.");
 		
 		if (Main.cmdLine != null) {
 			Main.cmdLine.setEditable(false);
@@ -85,5 +123,90 @@ public class JFxWinloader extends Application {
 	
 	public void clearCmdLine() {
 		Main.cmdLine.clear();
+	}
+	
+	/**
+	 * This piece of code is responsible for doing the work,
+	 * when enter has been pressed (meaning a new command has been submitted).
+	 */
+	
+	public void actionOnEnter() {
+		main.Main.tabCountInRow = 0;
+		//UPDATE SHELL STREAM ==============================================================================
+		Main.ThreadAllocMain.getSWT().updateShellStream();
+		//END UPDATE SHELL STREAM ==========================================================================
+		//Splitting WindowMain.cmdLine text into command
+		String[] lines = Main.cmdLine.textProperty().get().split("\n");
+		
+		String lastLine = lines[lines.length - 1];
+		
+		System.err.println("VarLib prompt: " + VarLib.getPrompt());
+		System.err.println("Last line length: " + lastLine.length());
+		System.err.println("Last line content: " + lastLine);
+		System.err.println("Prompt length: " + VarLib.getPrompt().length());
+		
+		//Extract full command from last line (Remove prompt)
+		//Dev. note: VarLib.getPrompt() contains ANSI excapes, but cmdLine.getText() doesn't, so
+		//all ANSI escape chars had to be cleared out by the regex shown.
+		String fullCommand = lastLine.substring(
+				VarLib.getPrompt().replaceAll("\u001B\\[[\\d;]*[^\\d;]","").length(), lastLine.length());
+		
+		//if (fullCommand.contains(VarLib.getPrompt())) { fullCommand = fullCommand.split("\\$ ")[1]; }
+		if (!fullCommand.isBlank()) {
+			if (fullCommand.contains(" && ")) {
+				sys.log("MAIN", 2, "Info: Found multiple commands connected with '&&'.");
+				sys.log("MAIN", 2, "This is still experimental: Expect errors.");
+				sys.shellPrintln("Using experimental command interconnect: '&&'");
+				for (String subCommand : fullCommand.split(" && ")) {
+					sys.log("MAIN", 0, "Running '" + fullCommand + "'");
+					sys.log("Subcommand: " + subCommand);
+					try {
+						components.Command cmd = new components.Command(subCommand);
+						cmd.start();
+						sys.log("New thread started (subCommand placed into cmdQueue)");
+						//For returnVal, try:
+						//CommandMain.executeCommand(new components.Command(fullCommand));
+					} catch (Exception ex) {
+						//Error information is printed to stdout and shell
+						Err.shellPrintErr(ex, "FATAL ERROR", "Non-caught JVM exception in class CmdMain");
+					}
+				}
+			} else {
+				sys.log("MAIN", 0, "Sending '" + fullCommand + "' to Command Parser");
+				try {
+					new components.Command(fullCommand).start();
+					//For returnVal, try:
+					//CommandMain.executeCommand(new components.Command(fullCommand));
+				} catch (Exception ex) {
+					Err.shellPrintErr(ex, "FATAL ERROR", "Non-caught JVM exception in class CmdMain");
+				}
+			}
+			//=========================ADD FULLCMD TO HISTORY===============================
+			main.Main.commandHistory.add(fullCommand);
+			try {
+				String history = Files.readString(Paths.get(
+						VarLib.getDataDir().getAbsolutePath() + VarLib.fsep + "cmd_history"));
+				int max_history_size = Integer.parseInt(Files.readString(Paths.get(
+						VarLib.getDataDir().getAbsolutePath() + VarLib.fsep + "cmd_history_max_length")).trim());
+				//Remove first entry of history until size of entries is below count in cmd_history_max_length
+				while (history.split("\n").length > max_history_size) {
+					Files.writeString(Paths.get(
+							VarLib.getDataDir().getAbsolutePath() + VarLib.fsep + "cmd_history"),
+							history.replaceFirst(history.split("\n")[0], ""),
+							StandardOpenOption.WRITE);
+				}
+				Files.writeString(Paths.get(
+						VarLib.getDataDir().getAbsolutePath() + VarLib.fsep + "cmd_history"),
+						fullCommand + "\n", StandardOpenOption.APPEND);
+			} catch (IOException ioe) {
+				sys.log("MAIN", 2, "IOException while writing to cmd history.");
+			} catch (NumberFormatException nfe) {
+				sys.log("MAIN", 2, "Parsing cmd_history_max_length failed. Check file exists" +
+						" and contains a number below 2.147.483.647");
+			}
+			//============================END ADD FULLCMD TO HISTORY==============================
+		} else {
+			OpenLib.cmdLinePrepare();
+		}
 	}
 }
