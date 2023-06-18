b245b9be7367a5ed305c6b46e0696bdf7300edb0
nick16384 <theophil@pudelkern.com>
2023-06-18 09:14:25 +0200

    Change-Id: I0000000000000000000000000000000000000000
    Change-Id: I9fb84825007a3a48fc4f328b72e650d0e01f0bd9

--- J-Venus/src/jfx/windowManager/PartlyEditableInlineCSSTextArea.java
+++ J-Venus/src/jfx/windowManager/PartlyEditableInlineCSSTextArea.java
@@ -1,8 +1,12 @@
 package jfx.windowManager;
 
+import java.util.Map;
+import java.util.HashMap;
 import org.fxmisc.richtext.InlineCssTextArea;
 
 import engine.sys;
+import javafx.application.Platform;
+import main.Main;
 
 /**
  * A normal InlineCssTextArea with the additional feature of being able to partly make the text read-only.
@@ -13,29 +17,42 @@ import engine.sys;
 public class PartlyEditableInlineCSSTextArea extends InlineCssTextArea {
 	private Thread CheckOverrideThread;
 	private int readOnlyToIndex;
+	
 	private String lastWrittenText;
 	private String currentShellText;
+	private String textUntilWritable;
 	
 	public PartlyEditableInlineCSSTextArea(String title) {
 		super(title);
 		readOnlyToIndex = 0;
+		lastWrittenText = "";
+		currentShellText = "";
+		textUntilWritable = "";
+		
+		
+		// This thread checks, whether the user has overridden read-only text and replaces it again with the original
 		CheckOverrideThread = new Thread(() -> {
-			while (true) {
-				try { Thread.sleep(50); } catch (InterruptedException ie) { ie.printStackTrace(); }
+			while (!Main.ThreadAllocMain.isShutdownSignalActive()) {
+				try { Thread.sleep(500); } catch (InterruptedException ie) { ie.printStackTrace(); }
 				sys.log("PartlyEditable text area scan");
 				// TODO check if a part has been edited and revert
 				// TODO add this to main class, otherwise there will be no effect
 				// FIXME message does not appear (thread is somehow still inactive)
 				
-				String textUntilWritable = this.getText().substring(0, readOnlyToIndex);
-				
-				if (!this.getText().startsWith(currentShellText)) {
-					this.clear();
-					this.appendText(lastWrittenText);
+				if (!this.getText().startsWith(textUntilWritable)) {
+					Platform.runLater(() -> {
+						Map<Integer, String> styles = new HashMap<Integer, String>();
+						styles = saveStyle();
+						
+						sys.log("Read-only text was affected. Reverting.");
+						this.clear();
+						this.appendText(textUntilWritable);
+						
+						reapplyStyle(styles);
+					});
 				}
 			}
 		});
-		
 		CheckOverrideThread.start();
 	}
 	
@@ -43,6 +60,8 @@ public class PartlyEditableInlineCSSTextArea extends InlineCssTextArea {
 	public void appendText(String text) {
 		lastWrittenText = text;
 		currentShellText += text;
+		updateReadOnlyToIndex();
+		textUntilWritable = currentShellText.substring(0, readOnlyToIndex);
 		super.appendText(text);
 	}
 	
@@ -56,6 +75,11 @@ public class PartlyEditableInlineCSSTextArea extends InlineCssTextArea {
 		return lastWrittenText;
 	}
 	
+	private void updateReadOnlyToIndex() {
+		if (readOnlyToIndex > currentShellText.length())
+			readOnlyToIndex = currentShellText.length();
+	}
+	
 	public int getReadOnlyToIndex() {
 		return readOnlyToIndex;
 	}
@@ -66,6 +90,23 @@ public class PartlyEditableInlineCSSTextArea extends InlineCssTextArea {
 			readOnlyToIndex = newReadOnlyToIndex;
 		} else {
 			sys.log("CLASS:CSSTextArea", 3, "new read-only length index " + newReadOnlyToIndex + " out of bounds.");
+		}
+	}
+	
+	// FIXME saveStyle() and reapplyStyle() might be very resource intensive. Maybe change or remove later.
+	private Map<Integer, String> saveStyle() {
+		Map<Integer, String> styles = new HashMap<Integer, String>();
+		for (int i = 0; i < this.getText().length(); i++) {
+			sys.log("Style saving progress: " + i + "/" + (this.getText().length() - 1));
+			styles.put(i, this.getStyleAtPosition(i));
+		}
+		return styles;
+	}
+	
+	private void reapplyStyle(Map<Integer, String> styles) {
+		for (int i = 0; i < this.getText().length() - 1; i++) {
+			sys.log("Style loading progress: " + i + "/" + (this.getText().length() - 1));
+			this.setStyle(i > 0 ? i - 1 : i, i, styles.get(i));
 		}
 	}
 }
